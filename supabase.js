8e896b2a40c1731c0a2a8cc64eb3ad0834f051e0
Namankumar517 <namangola517@gmail.com>
2025-11-24 18:07:23 +0530

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -4,7 +4,7 @@ console.log("SUPABASE LOADED");
 const SUPABASE_URL = "https://gqkklssatkwpgqhgqymz.supabase.co";
 
 // PASTE your FULL anon key here (from Supabase Dashboard -> Settings -> API)
-const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imdxa2tsc3NhdGt3cGdxaGdxeW16Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjM5MTMwMDcsImV4cCI6MjA3OTQ";
+const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imdxa2tsc3NhdGt3cGdxaGdxeW16Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjM5MTMwMDcsImV4cCI6MjA3OTQ4OTAwN30.M0BnJSwNDAU90wIqSxi_gBU0tyutvLNxBTHkM-YyVpc";
 
 const sb = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
 window.supabase = sb;
